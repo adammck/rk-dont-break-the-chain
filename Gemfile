b399c4b3e864ccf0c52e8a136b796abd04975e5a
adammck <adam.mckaig@gmail.com>
2012-05-11 13:16:49 -0400

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# vim: et ts=2 sw=2
+
+source "https://rubygems.org"
+
+gem "sinatra", "~> 1.3.2"
+gem "sass", "~> 3.1.17"
+gem "health_graph", :git=>"git://github.com/adammck/health_graph.git", :branch=>"edge"
+
+gem "thin", "~> 1.3.1"
+gem "shotgun", "~> 0.9"
---
+++ app.rb
@@ -0,0 +1,147 @@
+#!/usr/bin/env ruby
+# vim: et ts=2 sw=2
+
+require "set"
+require "date"
+require "health_graph"
+require "sinatra"
+require "sass"
+
+
+HealthGraph.configure do |config|
+  config.client_id = ENV["CLIENT_ID"]
+  config.client_secret = ENV["CLIENT_SECRET"]
+  config.authorization_redirect_url = ENV["REDIRECT_URL"]
+end
+
+
+helpers do
+  def is_checked day
+    @checked_days.include? day.date
+  end
+end
+
+
+get "/" do
+  token = request.cookies["token"]
+  redirect "/auth" unless token
+
+  user = HealthGraph::User.new(token)
+  @checked_days = get_checked_days!(user)
+  @month = Month.new(Date.today)
+
+  erb :calendar
+end
+
+get "/style.css" do
+  scss :style
+end
+
+get "/auth" do
+  redirect HealthGraph.authorize_url
+end
+
+get "/callback" do
+  response.set_cookie "token", HealthGraph.access_token(params[:code])
+  redirect "/"
+end
+
+
+def get_checked_days! user
+  Set.new.tap do |days|
+    feed = user.fitness_activities
+
+    while feed
+      feed.items.each do |item|
+        days.add item.start_time.to_date
+      end
+
+      feed = feed.next_page
+    end
+  end
+end
+
+
+class Month
+  def initialize date
+    @prng = Random.new(date.month)
+    @date = date
+  end
+
+  def weeks
+    first_day.step(last_day, 7).map do |date|
+      Week.new date
+    end
+  end
+
+  def weekdays
+    sunday = Date.today - Date.today.wday
+    sunday.upto(sunday + 6).map do |date|
+      Day.new date
+    end
+  end
+
+  def include? date
+    (date.year == @date.year) and (date.month == @date.month)
+  end
+
+  def strftime fmt
+    @date.strftime fmt
+  end
+
+  def random_angle
+    20 - @prng.rand(40)
+  end
+
+  private
+
+  def first_day
+    DateTime.new @date.year, @date.month, 1
+  end
+
+  def last_day
+    first_day.next_month.prev_day
+  end
+end
+
+class Week
+  def initialize date
+    @date = date
+  end
+
+  def days
+    first_day.upto(last_day).map do |date|
+      Day.new(date)
+    end
+  end
+
+  private
+
+  def first_day
+    @date - @date.wday
+  end
+
+  def last_day
+    first_day + 6
+  end
+end
+
+class Day
+  attr_reader :date
+
+  def initialize date
+    @date = date
+  end
+
+  def today?
+    @date == Date.today
+  end
+
+  def weekend?
+    @date.saturday? or @date.sunday?
+  end
+
+  def method_missing method_name, *args, &block
+    @date.send method_name, *args, &block
+  end
+end
